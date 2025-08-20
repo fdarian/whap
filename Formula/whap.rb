e2c40b4afc12eacf802f6efcd7d4d50275ccece1
GitHub Action <action@github.com>
2025-08-20 05:19:24 +0000

    chore: update Homebrew formula for version 0.1.0

--- Formula/whap.rb
+++ Formula/whap.rb
@@ -3,7 +3,7 @@ class Whap < Formula
   homepage "https://github.com/fdarian/whap"
   version "0.1.0"
   url "https://github.com/fdarian/whap/releases/download/v0.1.0/whap-darwin-x64.tar.gz"
-  sha256 "placeholder_sha256_will_be_updated_by_ci"
+  sha256 "43b65a58761bf5659247d182c1ec1afe3d5a4130b88084338c9080267c6710e9"
 
   def install
     bin.install "whap"
